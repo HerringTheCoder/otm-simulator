22119d086f80c9d61fdf1f7c7e027b74dc285ffa
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-07-15 13:37:11 +0200

    Added wip unit/integration testing

---
+++ otm-simulator-tests/StatesGeneratorTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using otm_simulator.Hubs;
+using otm_simulator.Interfaces;
+using otm_simulator.Services;
+using otm_simulator.Tests;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace otm_simulator_tests
+{
+    [TestClass]
+    public class StatesGeneratorTest
+    {
+        [TestMethod]
+        public async Task StateGenerator_Executes_Properly()
+        {
+
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole()
+                    .AddEventLog();
+            });
+            ILogger workerLogger = loggerFactory.CreateLogger<BackgroundWorker>();
+            ILogger stateLogger = loggerFactory.CreateLogger<StateGeneratorService>();
+            //Add dependencies
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IHostedService, BackgroundWorker>();
+            services.AddSingleton<ITimetableProvider, TimetableProviderMock>();
+            services.AddOptions();         
+            services.AddSingleton(loggerFactory);
+            services.AddSingleton(Mock.Of<IStateGenerator>());
+            var serviceProvider = services.AddLogging().BuildServiceProvider();
+
+            var hostedService = serviceProvider.GetService<IHostedService>();
+
+            //Start execution
+            await hostedService.StartAsync(CancellationToken.None);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await hostedService.StopAsync(CancellationToken.None);
+
+            var stateGeneratorService = serviceProvider.GetRequiredService<IStateGenerator>();
+
+            var mock = Mock.Get(stateGeneratorService);
+
+            mock.Verify(_ => _.CreateStates(), Times.AtLeastOnce);
+        }
+    }
+}
--- otm-simulator-tests/TimetableProviderMock.cs
+++ otm-simulator-tests/TimetableProviderMock.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using otm_simulator.Interfaces;
+﻿using otm_simulator.Interfaces;
 using otm_simulator.Models;
 using System;
 using System.Collections.Generic;
